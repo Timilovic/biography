9ddebcf60385165ec375fbb4b7fa9a8b44cb8c1a
Timilovic <dedeketimilehin@gmail.com>
2024-09-18 00:04:19 +0100

    done

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let button = document.querySelector("button");
+// let button = document.querySelector("button");
 
-button.addEventListener("click", function () {alert(new Date());
+// button.addEventListener("click", function () {alert(new Date());
 
+// });
+
+document.getElementById('button').addEventListener('click', function() {
+    // Change content in the <h4> tag
+    document.querySelector('#dev h4').textContent = 'Front-End Web Developer | Freelancer';
+    
+    // Change content in the <p> tag (location)
+    document.getElementById('ab').textContent = 'Abeokuta, Ogun State, Nigeria';
+
+    // Change the content in the <p> tag under the main section
+    document.getElementById('ch').textContent = 
+        'is a skilled front-end web developer, specializing in creating responsive and user-friendly websites and web applications. Based in Abeokuta, Ogun State, Nigeria, Timilehin has honed his expertise in HTML, CSS (including Grid and Flexbox layouts), and JavaScript, frequently using frameworks like Bootstrap to bring visually appealing designs to life.';
 });
+ 
