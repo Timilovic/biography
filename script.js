848568e7e8198168fb53ce5f81ec489816a780f1
Timilovic <dedeketimilehin@gmail.com>
2024-09-18 16:27:04 +0100

    done

--- script.js
+++ script.js
@@ -14,5 +14,8 @@ document.getElementById('button').addEventListener('click', function() {
     // Change the content in the <p> tag under the main section
     document.getElementById('ch').textContent = 
         'is a skilled front-end web developer, specializing in creating responsive and user-friendly websites and web applications. Based in Abeokuta, Ogun State, Nigeria, Timilehin has honed his expertise in HTML, CSS (including Grid and Flexbox layouts), and JavaScript, frequently using frameworks like Bootstrap to bring visually appealing designs to life.';
+    
+    document.querySelector('.bio').textContent = 'Biography' ;
+    document.querySelector('Skills')
 });
  
